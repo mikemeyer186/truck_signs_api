cc9daca596ba7c331df3b5087b6c8f58c13b6f04
Mike Meyer <mike.meyer86@web.de>
2024-09-26 21:38:07 +0200

    project: change settings and port

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ python manage.py migrate
 python manage.py makemigrations
 python create_superuser.py --noinput
 
-gunicorn truck_signs_designs.wsgi:application --bind 0.0.0.0:8000
+gunicorn truck_signs_designs.wsgi:application --bind 0.0.0.0:8020
 
 echo "Postgresql migrations finished"
 
